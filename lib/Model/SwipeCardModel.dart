9d3b5e33248a4598266dc8e9130348f98b2e1744
aaravs1031 <asahni_be21@thapar.edu>
2022-10-16 01:02:05 +0530

    added SwipeCard and Dots

---
+++ lib/Model/SwipeCardModel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SwipeCard {
+  final int id;
+  final String title;
+  final String content;
+
+  const SwipeCard(
+      {required this.id, required this.title, required this.content});
+}
